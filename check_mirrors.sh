53f0adea1dc98c23d8aa060ac5bc361c030a66ac
Qoqnus master <mahisokhary@gmail.com>
2026-05-03 08:59:51 +0330

    Improved checking dependencies
    By adding check_dependency function which check if given command exists
    if doesnt exists it will print a message (default message or given
    message)
    and exit with code 1

--- check_mirrors.sh
+++ check_mirrors.sh
@@ -4,19 +4,21 @@ set -euo pipefail
 MIRROR_FILE="./mirrors_list.yaml"
 MIRROR_URL="https://raw.githubusercontent.com/MiravaOrg/Mirava/refs/heads/main/mirrors_list.yaml"
 
-# Check if curl is installed
-if ! command -v curl &> /dev/null; then
-  echo "❌ Error: curl is not installed."
-  echo "Please install curl first."
-  exit 1
-fi
+function check_dependency() {
+	if ! command -v $1 &> /dev/null; then
+		echo "❌ Error: '$1' is not installed."
+		if [[ $# -gt 1 ]]; then
+			shift
+			echo $@
+		else
+			echo "Please install '$1' first."
+		fi
+		exit 1
+	fi
+}
 
-# Check if yq is installed
-if ! command -v yq &> /dev/null; then
-  echo "❌ Error: yq is not installed."
-  echo "Please install yq from: https://github.com/mikefarah/yq/"
-  exit 1
-fi
+check_dependency curl
+check_dependency yq Please install yq from: https://github.com/mikefarah/yq/
 
 # Check if mirrors_list.yaml exists, if not fetch it
 if [[ ! -f "$MIRROR_FILE" ]]; then
